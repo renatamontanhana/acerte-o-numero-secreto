ea37c2252572fc3bf411fe314fb6ff550fe78aff
Renata Montanhana <112817008+renatamontanhana@users.noreply.github.com>
2023-02-15 11:57:40 -0400

    Add files via upload

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,20 +1,24 @@
-const elementoChute = document.getElementById("chute");
+const elementoChute = document.getElementById('chute')
 
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-recognition.lang = "pt-Br";
-recognition.start();
-recognition.addEventListener("result", onSpeak);
+recognition.lang = 'pt-Br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
 
 function onSpeak(e) {
-  chute = e.results[0][0].transcript;
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+    verificaSeOChutePossuiUmValorValido(chute)
+}
 
-  exibeChuteNaTela(chute);
-}
 function exibeChuteNaTela(chute) {
-  elementoChute.innerHTML = `
-<div>Você disse</div>
-<span class="box">${chute}</span>
-`;
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+     `
 }
+
+recognition.addEventListener('end', () => recognition.start())
